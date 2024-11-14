60a2b757d4f5e82b0350b8df089755132f88d829
AC111433 <ac111433@hc.ac.je>
2024-11-14 15:42:57 +0000

    Navbar and Footer

--- app/root.tsx
+++ app/root.tsx
@@ -2,7 +2,8 @@ import { type LinksFunction } from '@remix-run/node'
 import Document from '~/components/shared-layout/Document'
 import { useNonce } from '~/utils/nonce-provider.ts'
 import rootLinkElements from '~/utils/providers/rootLinkElements'
-
+import HeaderWithSearch from './components/organisms/HeaderWithSearch'
+import FooterMenuRight from './components/organisms/Footer/FooterMenuRight'
 export const links: LinksFunction = () => {
 	return rootLinkElements
 }
@@ -16,11 +17,14 @@ export default function App() {
 	  <Document nonce={nonce}>
 		<div className="flex h-screen flex-col justify-between">
 		  <div className="flex-1">
+		  <HeaderWithSearch />
 			<main className="grid h-full place-items-center">
 			  <h1 className="text-mega">Welcome to Epic News!</h1>
 			</main>
+			<FooterMenuRight />
 		  </div>
 		</div>
 	  </Document>
+	  
 	)
   }
