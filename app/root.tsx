5c9018726bd40f43c957f1af0d20a20fcd7c17e5
AC111433 <ac111433@hc.ac.je>
2024-11-14 16:30:36 +0000

    Added logo to header

--- app/root.tsx
+++ app/root.tsx
@@ -2,8 +2,9 @@ import { type LinksFunction } from '@remix-run/node'
 import Document from '~/components/shared-layout/Document'
 import { useNonce } from '~/utils/nonce-provider.ts'
 import rootLinkElements from '~/utils/providers/rootLinkElements'
-import HeaderWithSearch from './components/organisms/HeaderWithSearch'
 import FooterMenuRight from './components/organisms/Footer/FooterMenuRight'
+import HeaderWithSearch from './components/organisms/HeaderWithSearch'
+
 export const links: LinksFunction = () => {
 	return rootLinkElements
 }
@@ -12,19 +13,18 @@ export { action, loader } from './__root.server.tsx'
 
 export default function App() {
 	const nonce = useNonce()
-  
+
 	return (
-	  <Document nonce={nonce}>
-		<div className="flex h-screen flex-col justify-between">
-		  <div className="flex-1">
-		  <HeaderWithSearch />
-			<main className="grid h-full place-items-center">
-			  <h1 className="text-mega">Welcome to Epic News!</h1>
-			</main>
-			<FooterMenuRight />
-		  </div>
-		</div>
-	  </Document>
-	  
+		<Document nonce={nonce}>
+			<div className="flex h-screen flex-col justify-between">
+				<div className="flex-1">
+					<HeaderWithSearch />
+					<main className="grid h-full place-items-center">
+						<h1 className="text-mega">Your Journey Begins!</h1>
+					</main>
+					<FooterMenuRight />
+				</div>
+			</div>
+		</Document>
 	)
-  }
+}
