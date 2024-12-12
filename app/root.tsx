2c77b30ee0033757574ac505e92850ce3fcb2521
AC111433 <ac111433@hc.ac.je>
2024-12-12 14:38:17 +0000

    Finished all of Security 101

--- app/root.tsx
+++ app/root.tsx
@@ -1,13 +1,15 @@
 import { type LinksFunction } from '@remix-run/node'
 import { Outlet, useLoaderData } from '@remix-run/react'
-import heroImage from '~/assets/jpg/sample-hero.jpg'
+import { AuthenticityTokenProvider } from 'remix-utils/csrf/react'
+import { HoneypotProvider } from 'remix-utils/honeypot/react'
 import Document from '~/components/shared-layout/Document'
 import { useNonce } from '~/utils/nonce-provider.ts'
 import rootLinkElements from '~/utils/providers/rootLinkElements'
-import { Button } from './components/atoms/Button.tsx'
+import { type loader } from './__root.server.tsx'
 import FooterMenuRight from './components/organisms/Footer/FooterMenuRight'
 import HeaderWithSearch from './components/organisms/HeaderWithSearch'
-import HeroCallToAction from './components/organisms/Hero/HeroCallToAction.tsx'
+import { useToast } from './components/toaster.tsx'
+import useTheme from './hooks/useTheme.tsx'
 export const links: LinksFunction = () => {
 	return rootLinkElements
 }
@@ -15,19 +17,27 @@ export { headers, meta } from './__root.client.tsx'
 export { action, loader } from './__root.server.tsx'
 
 export default function App() {
+	const data = useLoaderData<typeof loader>()
 	const nonce = useNonce()
+	const theme = useTheme()
+
+	useToast(data.toast)
 
 	return (
-		<Document nonce={nonce}>
-			<div className="flex h-screen flex-col justify-between">
-				<div className="flex-1">
-					<HeaderWithSearch />
+		<AuthenticityTokenProvider token={data.csrfToken}>
+			<HoneypotProvider {...data.honeyProps}>
+				<Document nonce={nonce} theme={theme}>
+					<div className="flex h-screen flex-col justify-between">
+						<HeaderWithSearch />
 
-					<Outlet />
+						<div className="flex-1">
+							<Outlet />
+						</div>
 
-					<FooterMenuRight />
-				</div>
-			</div>
-		</Document>
+						<FooterMenuRight />
+					</div>
+				</Document>
+			</HoneypotProvider>
+		</AuthenticityTokenProvider>
 	)
 }
