8bd1a9ff408bbdf2ffa672573c64ef657c632d80
AC111433 <ac111433@hc.ac.je>
2025-01-09 16:28:49 +0000

    Finished create a single article page

--- app/routes/news.$category.tsx
+++ app/routes/news.$category.tsx
@@ -2,13 +2,17 @@ import { invariant } from '@epic-web/invariant'
 import { type LoaderFunctionArgs, json } from '@remix-run/node'
 import { useLoaderData } from '@remix-run/react'
 import { prisma } from '~/utils/db.server.ts'
-import { toTitleCase } from '~/utils/stringUtils.ts'
+
 export async function loader({ params }: LoaderFunctionArgs) {
 	const { category } = params
 	invariant(typeof category === 'string', 'Category not found')
 
-	const categoryTitle = toTitleCase(category)
-	const allArticles = await prisma.article.findMany({
+	const filteredArticles = await prisma.article.findMany({
+		where: {
+			category: {
+				slug: category, // Retrieves only articles in the specified category
+			},
+		},
 		select: {
 			id: true,
 			title: true,
@@ -16,16 +20,16 @@ export async function loader({ params }: LoaderFunctionArgs) {
 			images: { select: { id: true } },
 		},
 	})
-	return json({ categoryTitle, allArticles })
+	return json({ category, filteredArticles })
 }
 
 export default function NewsCategoryPage() {
-	const { categoryTitle, allArticles } = useLoaderData<typeof loader>()
+	const { category, filteredArticles } = useLoaderData<typeof loader>()
 	return (
 		<div className="container py-16">
-			<h2 className="mb-6 text-h2">{categoryTitle}</h2>
+			<h2 className="mb-6 text-h2">{category}</h2>
 			<div className="grid grid-cols-2 gap-6 md:grid-cols-3 lg:grid-cols-5">
-				{allArticles.map(article => (
+				{filteredArticles.map(article => (
 					<div
 						className="h-full rounded-full bg-red-600 p-4 hover:bg-red-950"
 						key={article.id}
